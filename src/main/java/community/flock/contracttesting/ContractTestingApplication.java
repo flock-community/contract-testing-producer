c1392d0a6bcf14f80b59dd254a5ca4bf29fb5905
Jacky To <cyto1984@gmail.com>
2020-02-28 11:23:33 +0100

    Initialize repository

---
+++ src/main/java/community/flock/contracttesting/ContractTestingApplication.java
@@ -0,0 +1,13 @@
+package community.flock.contracttesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractTestingApplication.class, args);
+	}
+
+}
